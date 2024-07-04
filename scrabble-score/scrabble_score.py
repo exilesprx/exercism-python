453980e8fd78eef34680c7ca7f6a74f31dff7516
Exiles.prx <exiles.prx@gmail.com>
2024-07-04 18:20:37 -0400

    Solved exercise scrabble score
    Aimed to solve in a functional manner

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,2 +1,20 @@
 def score(word):
-    pass
+    return sum(map(lambda x: letter_value(x.lower()), word))
+
+
+def letter_value(letter):
+    if letter in "aeioulnrst":
+        return 1
+    if letter in "dg":
+        return 2
+    if letter in "bcmp":
+        return 3
+    if letter in "fhvwy":
+        return 4
+    if letter in "k":
+        return 5
+    if letter in "jx":
+        return 8
+    if letter in "qz":
+        return 10
+    return 0

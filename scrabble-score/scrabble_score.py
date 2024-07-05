fc4563e3a7034406c3364e91c0cd2438881198bb
Exiles.prx <exiles.prx@gmail.com>
2024-07-05 12:43:29 -0400

    Optimized using dictionary

--- scrabble-score/scrabble_score.py
+++ scrabble-score/scrabble_score.py
@@ -1,20 +1,13 @@
+letter_values = {
+    **dict.fromkeys("aeioulnrst", 1),
+    **dict.fromkeys("dg", 2),
+    **dict.fromkeys("bcmp", 3),
+    **dict.fromkeys("fhvwy", 4),
+    **dict.fromkeys("k", 5),
+    **dict.fromkeys("jx", 8),
+    **dict.fromkeys("qz", 10),
+}
+
+
 def score(word):
-    return sum(map(lambda x: letter_value(x.lower()), word))
-
-
-def letter_value(letter):
-    if letter in "aeioulnrst":
-        return 1
-    if letter in "dg":
-        return 2
-    if letter in "bcmp":
-        return 3
-    if letter in "fhvwy":
-        return 4
-    if letter in "k":
-        return 5
-    if letter in "jx":
-        return 8
-    if letter in "qz":
-        return 10
-    return 0
+    return sum(map(lambda x: letter_values.get(x.lower(), 0), word))

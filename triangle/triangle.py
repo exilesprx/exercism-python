e2b9859aa4c6fdc189dcf73802260447ca3bd84a
Exiles.prx <exiles.prx@gmail.com>
2024-08-14 12:38:04 -0400

    Solved exercise triangle

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,10 +1,21 @@
 def equilateral(sides):
-    pass
+    a, b, c = sides
+    return (a == b == c) and _is_triangle(a, b, c)
 
 
 def isosceles(sides):
-    pass
-
+    a, b, c = sides
+    return (a == b or a == c or b == c) and _is_triangle(a, b, c)
 
 def scalene(sides):
-    pass
+    a, b, c = sides
+    return (a != b and b != c and a != c) and _is_triangle(a, b, c)
+
+def _is_triangle(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return False
+
+    if a + b <= c or b + c <= a or a + c <= b:
+        return False
+
+    return True

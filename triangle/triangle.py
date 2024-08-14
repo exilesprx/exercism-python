f0c86db3660dd632c19a51d26b006f1784d7dd56
Exiles.prx <exiles.prx@gmail.com>
2024-08-14 12:53:38 -0400

    Fix formatting

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -7,9 +7,11 @@ def isosceles(sides):
     a, b, c = sides
     return (a == b or a == c or b == c) and _is_triangle(a, b, c)
 
+
 def scalene(sides):
     a, b, c = sides
     return (a != b and b != c and a != c) and _is_triangle(a, b, c)
+
 
 def _is_triangle(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:

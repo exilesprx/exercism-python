e2a02a789286f1ee046ea90e8b9134d74e53ed37
Exiles.prx <exiles.prx@gmail.com>
2024-08-20 12:51:22 -0400

    Solved exercise collatz conjecture

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,16 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Only positive integers are allowed")
+
+    steps = 0
+    while True:
+        if number == 1:
+            return steps
+
+        elif number % 2 == 0:
+            number //= 2
+
+        else:
+            number = 3 * number + 1
+
+        steps += 1

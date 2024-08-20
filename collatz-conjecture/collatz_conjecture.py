a835860f2d801ced5faa1b36c4126f3fc601cd46
Exiles.prx <exiles.prx@gmail.com>
2024-08-20 12:55:57 -0400

    Pythonic answer

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -3,14 +3,8 @@ def steps(number):
         raise ValueError("Only positive integers are allowed")
 
     steps = 0
-    while True:
-        if number == 1:
-            return steps
-
-        elif number % 2 == 0:
-            number //= 2
-
-        else:
-            number = 3 * number + 1
-
+    while number != 1:
+        number = 3 * number + 1 if number % 2 else number // 2
         steps += 1
+
+    return steps

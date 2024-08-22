9e794becbe306c5bbe708e0f0c0e192460128a6b
Exiles.prx <exiles.prx@gmail.com>
2024-08-22 16:10:20 -0400

    Update to use "not" for empty string check

--- bob/bob.py
+++ bob/bob.py
@@ -1,11 +1,10 @@
 def response(hey_bob):
     hey_bob = str(hey_bob).strip()
-    if len(hey_bob) == 0:
-        return "Fine. Be that way!"
-
     is_question = hey_bob.endswith("?")
     is_yelling = hey_bob.isupper()
 
+    if not hey_bob:
+        return "Fine. Be that way!"
     if is_question and is_yelling:
         return "Calm down, I know what I'm doing!"
     elif is_question:

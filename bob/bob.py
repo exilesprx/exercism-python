01c2efd781240cdc6a1bb5954b9cdb275c72e089
Exiles.prx <exiles.prx@gmail.com>
2024-08-22 16:08:07 -0400

    Solved exercise bob

--- bob/bob.py
+++ bob/bob.py
@@ -1,2 +1,16 @@
 def response(hey_bob):
-    pass
+    hey_bob = str(hey_bob).strip()
+    if len(hey_bob) == 0:
+        return "Fine. Be that way!"
+
+    is_question = hey_bob.endswith("?")
+    is_yelling = hey_bob.isupper()
+
+    if is_question and is_yelling:
+        return "Calm down, I know what I'm doing!"
+    elif is_question:
+        return "Sure."
+    elif is_yelling:
+        return "Whoa, chill out!"
+
+    return "Whatever."

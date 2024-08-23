f04ebc775ddd1448e94f9eedce4eb9a1ce155711
Exiles.prx <exiles.prx@gmail.com>
2024-08-22 21:01:38 -0400

    Solved exercise darts

--- darts/darts.py
+++ darts/darts.py
@@ -1,2 +1,14 @@
+from math import sqrt
+
+
 def score(x, y):
-    pass
+    dart_distance = sqrt(x**2 + y**2)
+
+    if dart_distance <= 1:
+        return 10
+    elif dart_distance <= 5:
+        return 5
+    elif dart_distance <= 10:
+        return 1
+
+    return 0

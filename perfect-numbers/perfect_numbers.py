fdd3ac00898cb3f87c976c7e0bf00a9ec934ba60
Exiles.prx <exiles.prx@gmail.com>
2024-08-22 22:11:36 -0400

    Solved perfect numbers

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -1,7 +1,24 @@
+from math import ceil
+
+
 def classify(number):
-    """ A perfect number equals the sum of its positive divisors.
+    """A perfect number equals the sum of its positive divisors.
 
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number <= 0:
+        raise ValueError("Classification is only possible for positive integers.")
+
+    aliquot_sum = 0
+
+    for i in range(1, number):
+        if number % i == 0:
+            aliquot_sum += i
+
+    if number < aliquot_sum:
+        return "abundant"
+    elif number > aliquot_sum:
+        return "deficient"
+
+    return "perfect"

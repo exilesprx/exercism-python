b03caa1b64cccc66b770b40c736e561151030040
Exiles.prx <exiles.prx@gmail.com>
2024-08-30 23:10:40 -0400

    Use pythonic built-in method issubset

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,10 +1,4 @@
+import string
+
 def is_pangram(sentence):
-    letters = [0 for _ in range(26)]
-
-    for letter in sentence:
-        if not letter.isalpha():
-            continue
-
-        letters[ord(letter.lower()) - 97] = 1
-
-    return sum(letters) == 26
+    return set(string.ascii_lowercase).issubset(sentence.lower())

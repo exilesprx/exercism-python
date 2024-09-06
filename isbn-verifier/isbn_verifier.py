a0dada89646c310a8900b6469e2c467b531b4774
Exiles.prx <exiles.prx@gmail.com>
2024-09-06 00:50:54 -0400

    Solved isbn verifier

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,18 @@
 def is_valid(isbn):
-    pass
+    sum = 0
+    isbn = isbn.replace('-', '')
+
+    if len(isbn) != 10:
+        return False
+
+    for i, c in enumerate(isbn):
+        if not c.isdigit():
+            if c != 'X' or (c == 'X' and i != 9):
+                return False
+            else:
+                c = 10
+
+        sum += int(c) * (10 - i)
+
+    return sum % 11 == 0
+

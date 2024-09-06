bdd44ca167c22489cb100671d178a2ca5a0861b4
Exiles.prx <exiles.prx@gmail.com>
2024-09-06 01:00:54 -0400

    Update formatting

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,13 +1,13 @@
 def is_valid(isbn):
     sum = 0
-    isbn = isbn.replace('-', '')
+    isbn = isbn.replace("-", "")
 
     if len(isbn) != 10:
         return False
 
     for i, c in enumerate(isbn):
         if not c.isdigit():
-            if c != 'X' or (c == 'X' and i != 9):
+            if c != "X" or (c == "X" and i != 9):
                 return False
             else:
                 c = 10
@@ -15,4 +15,3 @@ def is_valid(isbn):
         sum += int(c) * (10 - i)
 
     return sum % 11 == 0
-
